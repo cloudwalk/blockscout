e40354751b95e7ad72b7e6c341ceed5aec0ab34b
Everest <caio.everest@cloudwalk.io>
2022-04-07 14:57:26 -0300

    big layout refactor

--- apps/block_scout_web/assets/js/pages/dark-mode-switcher.js
+++ apps/block_scout_web/assets/js/pages/dark-mode-switcher.js
@@ -1,11 +1,13 @@
 import $ from 'jquery'
 
 $('.dark-mode-changer').click(function () {
-  if (localStorage.getItem('current-color-mode') === 'dark') {
-    localStorage.setItem('current-color-mode', 'light')
-  } else {
-    localStorage.setItem('current-color-mode', 'dark')
-  }
+  // if (localStorage.getItem('current-color-mode') === 'dark') {
+  //   localStorage.setItem('current-color-mode', 'light')
+  // } else {
+  //   localStorage.setItem('current-color-mode', 'dark')
+  // }
   // reload each theme switch
+  // todo: revert chages on this file to enable darkmode again
+  localStorage.setItem('current-color-mode', 'light')
   document.location.reload(true)
 })
